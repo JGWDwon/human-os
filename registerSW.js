f6fc7c981eac289d5447a8c672f58cdb0105ed21
JGWDwon <JGWDwon@users.noreply.github.com>
2026-08-07 04:39:55 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/human-os/sw.js', { scope: '/human-os/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
